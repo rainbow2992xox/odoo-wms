413878ad8ef8702c2a2f22c58fd9af164ae4cf3c
rainbow2992xox <383171720@qq.com>
2022-09-06 05:43:35 +0800

    测试数据库配置文件

--- local/wms/__manifest__.py
+++ local/wms/__manifest__.py
@@ -3,12 +3,13 @@
     'version': '1.1.0',
     'depends': ['base'],
     'data': [
-             'views/vehicle.xml',
-             'views/api_log.xml',
-             'views/stock.xml',
-             'views/in_stock.xml',
-             'views/out_stock.xml',
-             'views/move_stock.xml'],
+        'views/stock.xml',
+        'views/in_stock.xml',
+        'views/out_stock.xml',
+        'views/move_stock.xml',
+        'views/vehicle.xml',
+        'views/api_log.xml'
+    ],
     'assets': {
         'web.assets_backend': [
             '/wms/static/src/css/style.scss',
--- local/wms/models/__init__.py
+++ local/wms/models/__init__.py
@@ -1,15 +1,9 @@
-# from . import address
-# from . import organization
-# from . import api_log
+from . import api_log
 from . import merchandise
-# from . import organization_car
-# from . import organization_employee
-# from . import region
 from . import stock
 from . import in_stock
 from . import out_stock
 from . import move_stock
 from . import warehouse_area
 from . import vehicle
-# from . import api_config
-# from . import api_view
+
