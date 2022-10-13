84b48385a8399fc59521f86c7405c9ab90ba08f9
rainbow2992xox <383171720@qq.com>
2022-10-13 14:36:20 +0800

    apilog

--- local/wms/models/api_log.py
+++ local/wms/models/api_log.py
@@ -11,8 +11,8 @@ import os
 import time
 
 
-class ApiLog(models.Model):
-    _name = 'wms.api.log'
+class ApiPost(models.Model):
+    _name = 'wms.api.post'
     # api_address = fields.Char('API地址')
     # status = fields.Selection([('0', '成功'), ('1', '失败')], string='状态')
     # body = fields.Text(string='请求结果')
