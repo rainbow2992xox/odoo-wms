2cc6a845097c6e36620b03c5239dd5b733794eee
rainbow2992xox <383171720@qq.com>
2022-10-13 15:28:01 +0800

    apilog

--- local/wms/models/api_log.py
+++ local/wms/models/api_log.py
@@ -13,10 +13,10 @@ import time
 
 class ApiLog(models.Model):
     _name = 'wms.api.log'
-#     api_address = fields.Char('API地址')
-#     status = fields.Selection([('0', '成功'), ('1', '失败')], string='状态')
-#     body = fields.Text(string='请求结果')
-#     res = fields.Text(string='返回结果')
+    api_address = fields.Char('API地址')
+    status = fields.Selection([('0', '成功'), ('1', '失败')], string='状态')
+    body = fields.Text(string='请求结果')
+    res = fields.Text(string='返回结果')
 #
 #     _sql = {
 #         "stock": '''SELECT si.FACILITY_ALIAS_ID 仓库号
