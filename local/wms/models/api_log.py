8b78750e2313e89532c5591b0c9f2122395f0ff9
rainbow2992xox <383171720@qq.com>
2022-12-20 15:19:51 +0800

    will_bill_code sql merchandise_file warehouse_area

--- local/wms/models/api_log.py
+++ local/wms/models/api_log.py
@@ -608,7 +608,7 @@ class ApiLog(models.Model):
             #              "res": res
             #              })
         elif key == "merchandise_files":
-            df = pd.read_excel("/opt/odoo-wms/local/wms/data/merchandise.xlsx", dtype={"危险货物类别": str})
+            df = pd.read_excel("/opt/odoo-wms/local/wms/data/merchandise.xlsx",sheet_name="wms_merchandise", dtype={"危险货物类别": str})
             code_list = list(set(df['CAS索引号']))
             for code in code_list:
                 # '/home/reagent/opt/sdspdf'
