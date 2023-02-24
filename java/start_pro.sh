56e805588fe7a407ae7c70d68ac925b57c05b571
rainbow2992xox <383171720@qq.com>
2023-02-24 14:22:47 +0800

    `java`

--- java/start_pro.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup java -jar -Dspring.config.location=./application_pro.yml sync.jar > pro.log 2>&1 &
--- java/start_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup java -jar -Dspring.config.location=./application_test.yml sync.jar > test.log 2>&1 &
